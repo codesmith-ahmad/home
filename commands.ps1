b1de5ee4002e2386ee46474cc058f88cfb71f763
The Codesmith <b-9k@outlook.com>
2024-03-14 14:43:24 -0400

    send to luminous

--- commands.ps1
+++ commands.ps1
@@ -1,15 +1,19 @@
-$cmds = 'D:\HOME\commands.ps1'
+# This script is launched from pwsh > $profile
+
+Write-Host "HELLO" $env:COMPUTERNAME `r`n
+
+$cmds = $pwd.path + "\commands.ps1"
 
 function help {
     echo "Commands:"
     echo "    `e[92mcmds`e[0m: modify commands"
     echo "    `e[92mnav`e[0m: Launch navigation"
-    echo "    `e[92mdb`e[0m: Connect to database"
+    echo "    `e[92mconnect`e[0m: Connect to database"
     echo "    `e[92mscriptorium`e[0m: Work in progress..."
     echo ""
-    echo "To modify commands: saps $cmds"
+    echo "To modify commands: saps $cmds`n"
 }
 function cmds {saps $cmds}
 function nav {python D:\GIT\nav-ninja\nav_ninja.py}
-function db {python D:\GIT\sqlite\root\main.py}
+function connect {python D:\GIT\sqlite\root\main.py}
 function scriptorium {python D:\GIT\scriptorium\root\main.py}
